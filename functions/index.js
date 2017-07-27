eba25ca782769b6bf7a608e686d39c2c9e6bb930
Joran Le Cren <joran.lecren@gmail.com>
2017-07-27 17:14:55 +0200

    Fetch the question from the database.

--- functions/index.js
+++ functions/index.js
@@ -7,17 +7,9 @@ admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.json({ "messages": [ { "text": "Hello from Firebase!" } ] });
-});
-
-const questions = [
-	"Do you take initiatives?",
-	"Do you take care of people?",
-	"Do you prefer to be alone?",
-	"Do you prefer to listen than to talk?",
-	"Do you feel rarely tired?"
-]
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.json({ "messages": [ { "text": "Hello from Firebase!" } ] });
+// });
 
 /*
 	Map the answer text to the answer code.
@@ -38,49 +30,129 @@ const answerCodeMapPerLang = {
 /*
 	Get the next 6 factors question given the previous question id
 	params:
+	 - "chat user id"   : the user id in Chatfuel
 	 - "questionId"   : the previous question id
+	 - "locale"       : the current user language
 */
 exports.sixfactorsGetNextQuestion = functions.https.onRequest((request, response) => {
 
   console.log("sixfactorsGetNextQuestion : " + JSON.stringify(request.query) );
   
+  // Grab the chatfuel user ID parameter.
+  const userId     = request.query["chatfuel user id"];
   // Grab the questionId parameter.
-  const questionId = request.query["questionId"];
-  // Parse the parameter as an integer
-  var questionIndex = parseInt(questionId, 10);
+  const lastQuestionId = request.query["questionId"];
+  // Grab the locale parameter.
+  const locale = request.query["locale"];
 
-  // Set 0 if it is not a number or increment the question index
-  if( isNaN(questionIndex) ) {
-  	questionIndex = 0;
-  } else {
-  	questionIndex++;
+  if( !verifyParam(userId) ) {
+  	badRequest(response, "Unable to find the user id.");
+  	return;
   }
 
+  if( !verifyParam(locale) ) {
+  	badRequest(response, "Unable to find the user locale.");
+  	return;
+  }
 
-  if( questionIndex >= 0 && questionIndex < questions.length ) {
+  const lang = getLang(locale);
+
+  // CALLBACKS : Create callbacks function to chain into a promise.
+  const __retrieveLastQuestionId = (userId) => {
+
+  	console.log("__retrieveLastQuestionId: userId = " + userId );
+
+  	const lastQuestionRef = admin.database().ref('/sixfactors/answers').child(userId).child("lastQuestionId");
+
+  	return lastQuestionRef.once("value")
+  	.then( (dataSnapshot) => {
+
+  		if( !dataSnapshot.exists() ) {
+  			return -1;
+  		}
+
+  		return dataSnapshot.val();
+
+  	} );
+  }
+
+  const __incrementQuestionId = (lastQuestionId) => {
+
+  	console.log("__incrementQuestionId: lastQuestionId = " + lastQuestionId );
+
+  	return lastQuestionId + 1;
+  }
+
+  const __endOfTest = (reason) => {
+
+  	console.log("__endOfTest: reason = " + reason);
+
+  	return {
+  			"isComplete": true,
+  			"id": -1,
+  			"label": ""
+  		}
+  };
+
+  const __fetchQuestion = (questionId) => {
+
+  	console.log("__fetchQuestion: questionId = " + questionId );
+
+  	const questionRef = admin.database().ref('/sixfactors/meta/questions').child(questionId);
+
+  	return questionRef.once('value')
+  	.then( (questionSnapshot) => {
+
+  		if( !questionSnapshot.exists() ) {
+  			return __endOfTest("The question doesn't exist.");
+  		}
+
+  		const questionLabel = questionSnapshot.child("label").child(lang).val();
+
+  		console.log("__fetchQuestion => id: " + questionId +" label: " + questionLabel );
+
+  		return {
+  			"isComplete": false,
+  			"id": questionId,
+  			"label": questionLabel
+  		}
+
+  	});
+  }
+
+  const __createResponse = (question) => {
+
+  	console.log("__createResponse: " + JSON.stringify(question) );
 
   	response.json( {
 		"set_attributes": 
 		{
-			"isComplete": false,
-			"questionId": questionIndex,
-			"questionText": questions[ questionIndex ],
-		}
-	});
-
-  } else {
-
-  	response.json( {
-		"set_attributes": 
-		{
-			"isComplete": true,
-			"questionId": questions.length,
-			"questionText": "",
+			"isComplete": question.isComplete,
+			"questionId": question.id,
+			"questionText": question.label,
 		}
 	});
 
   }
- 
+  // END CALLBACKS
+  
+
+  // Get the last question id for this user if the parameter is not valid
+  var lastQuestionIndex = parseInt(lastQuestionId, 10);
+  var promise;
+
+  if( isNaN(lastQuestionIndex) ) {
+  	promise = __retrieveLastQuestionId(userId)
+  } else {
+  	promise = new Promise( (resolve, reject) => {
+  		resolve(lastQuestionIndex);
+  	} );
+  }
+
+  promise
+  	.then(__incrementQuestionId)
+	.then(__fetchQuestion).catch(__endOfTest)
+	.then(__createResponse);
   
 });
 
@@ -131,7 +203,7 @@ exports.sixfactorsSaveAnswer = functions.https.onRequest((request, response) => 
   const answerCode = getAnswerCode(lang, userAnswer);
 
   var answer = {};
-  answer["lastQuestionId"] = questionId;
+  answer["lastQuestionId"] = parseInt(questionId, 10);
   answer[questionId] = answerCode;
 
   const userAnswersRef = admin.database().ref('/sixfactors/answers').child(userId);
